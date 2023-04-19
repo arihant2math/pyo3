1521f425d5977336f07a4f4bd7dc21f537a284f8
Ashwin Naren <arihant2math@gmail.com>
2023-04-18 19:19:02 -0700

    updates
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- examples/plugin/plugin_api/noxfile.py
+++
@@ -1,9 +0,0 @@
-import nox
-
-
-@nox.session
-def python(session):
-    session.install("-rrequirements-dev.txt")
-    session.install("maturin")
-    session.run_always("maturin", "develop", "--features", "extension-module")
-    session.run("pytest")
